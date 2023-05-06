9919acecbdd66c181867c863205fa3d30d3b7605
KKonaNN <87090666+KKonaNN@users.noreply.github.com>
2023-05-06 16:36:28 +0100

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,91 @@
+let USER_FLAGS = [
+    {
+        flag: "DISCORD_EMPLOYEE",
+        bitwise: 1 << 0,
+        id: 0
+    },
+    {
+        flag: "PARTNERED_SERVER_OWNER",
+        bitwise: 1 << 1,
+        id: 1
+    },
+    {
+        flag: "HYPESQUAD_EVENTS",
+        bitwise: 1 << 2,
+        id: 2
+    },
+    {
+        flag: "BUGHUNTER_LEVEL_1",
+        bitwise: 1 << 3,
+        id: 3
+    },
+    {
+        flag: "HOUSE_BRAVERY",
+        bitwise: 1 << 6,
+        id: 6
+    },
+    {
+        flag: "HOUSE_BRILLIANCE",
+        bitwise: 1 << 7,
+        id: 7
+    },
+    {
+        flag: "HOUSE_BALANCE",
+        bitwise: 1 << 8,
+        id: 8
+    },
+    {
+        flag: "EARLY_SUPPORTER",
+        bitwise: 1 << 9,
+        id: 9
+    },
+    {
+        flag: "TEAM_USER",
+        bitwise: 1 << 10,
+        id: 10
+    },
+    {
+        flag: "BUGHUNTER_LEVEL_2",
+        bitwise: 1 << 14,
+        id: 14
+    },
+    {
+        flag: "VERIFIED_BOT",
+        bitwise: 1 << 16,
+        id: 16
+    },
+    {
+        flag: "EARLY_VERIFIED_BOT_DEVELOPER",
+        bitwise: 1 << 17,
+        id: 17
+    },
+    {
+        flag: "DISCORD_CERTIFIED_MODERATOR",
+        bitwise: 1 << 18,
+        id: 18
+    },
+    {
+        flag: "BOT_HTTP_INTERACTIONS",
+        bitwise: 1 << 19,
+        id: 19
+    },
+    {
+        flag: "SPAMMER",
+        bitwise: 1 << 20,
+        id: 20
+    },
+    {
+        flag: "ACTIVE_DEVELOPER",
+        bitwise: 1 << 22,
+        id: 22
+    },
+    {
+        flag: "QUARANTINED",
+        bitwise: 17592186044416,
+        id: 63
+    }
+];
+
+module.exports = {
+    USER_FLAGS,
+}
---
+++ main.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express();
+const { USER_FLAGS } = require("./constant");
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+app.disable('x-powered-by');
+
+app.use(express.static('cdn'));
+
+app.use((req, res, next) => {
+    res.append("Access-Control-Allow-Origin", ["*"]);
+    res.append("Access-Control-Allow-Methods", "GET,POST");
+    res.append("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.status(200).render('index.ejs', { title: 'Home', found: false });
+});
+
+app.post('/fetch/user/:id', async (req, res) => {
+    let id = req.params.id;
+
+    try {
+        fetch(`https://canary.discord.com/api/v10/users/${id}`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bot ${process.env.TOKEN}`,
+            },
+        }).then((res) => res.json()).then((json) => {
+
+            if (json.id == undefined) {
+                let output = {
+                    id: id,
+                    created_at: snowflakeToUtc(id),
+                }
+                return res.status(200).render('index.ejs', { user: output, found: false });
+            }
+
+            let publicFlags = [];
+            USER_FLAGS.forEach((flag) => {
+                if (json.public_flags & flag.bitwise) {
+                    publicFlags.push(flag.id)
+                };
+            });
+
+            let avatarLink = null;
+            if (json.avatar) {
+                avatarLink = `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}`;
+            }
+
+            let bannerLink = null;
+            if (json.banner) {
+                bannerLink = `https://cdn.discordapp.com/banners/${json.id}/${json.banner}`;
+            }
+
+            let output = {
+                id: json.id,
+                created_at: snowflakeToUtc(json.id),
+                tag: `${json.username}#${json.discriminator}`,
+                badges: publicFlags,
+                avatar: {
+                    id: json.avatar,
+                    link: avatarLink,
+                    is_animated: json.avatar != null && json.avatar.startsWith("a_") ? true : false,
+                },
+                banner: {
+                    id: json.banner,
+                    link: bannerLink,
+                    is_animated: json.banner != null && json.banner.startsWith("a_") ? true : false,
+                    color: json.banner_color,
+                },
+                bot: json.bot,
+            }
+            res.status(200).render('index.ejs', { title: `${json.username}#${json.discriminator}`, user: output, found: true });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(404).render('index.ejs', { found : false, title: '404' });
+    }
+});
+
+function snowflakeToUtc(snowflakeId) {
+    const SNOWFLAKE_EPOCH = 1420070400000;
+    const timestamp = (snowflakeId / 4194304) + SNOWFLAKE_EPOCH;
+    const date = new Date(timestamp);
+    return date.toUTCString();
+}
+
+app.get('/bot/:id', (req, res) => {
+    let id = req.params.id;
+    res.redirect(`https://discord.com/oauth2/authorize?client_id=${id}&scope=bot&permissions=0`);
+});
+
+app.use((req, res) => {
+    res.status(404).render('index.ejs', { found : false, title: '404' });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+});
